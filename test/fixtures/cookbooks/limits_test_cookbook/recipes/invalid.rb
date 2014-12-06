262a930d5c4f6e6cb9dea3975492f60c2c9b96a8
Jordan Wesolowski <jrwesolo@gmail.com>
2014-12-06 11:59:32 -0700

    adjust test resource name to be shorter

--- test/fixtures/cookbooks/limits_test_cookbook/recipes/invalid.rb
+++ test/fixtures/cookbooks/limits_test_cookbook/recipes/invalid.rb
@@ -1,4 +1,4 @@
-limits_config 'invalid_with_extension.conf' do
+limits_config 'invalid.conf' do
   limits [
     { domain: 'phil', type: 'squishy', item: 'maxlogins', value: 5 },
     { domain: 'kyle', type: 'softish', item: 'nproc', value: 678 }
