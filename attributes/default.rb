f2c7a641a8f9af944379930a94b45b07b54e080d
Jordan Wesolowski <jrwesolo@gmail.com>
2014-12-06 12:02:55 -0700

    remove ['limits']['system_limits'] attribute
    This is no longer needed and the functionality has been removed. The
    usage of this cookbook will be only through LWRP or definitions.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,5 @@
 # for system limits.conf
 default['limits']['system_conf'] = '/etc/security/limits.conf'
-default['limits']['system_limits'] = []
 
 # for limits.d
 default['limits']['conf_dir'] = '/etc/security/limits.d'
